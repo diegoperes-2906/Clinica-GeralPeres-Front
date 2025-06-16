5d903f6a1aec397454b5cc7d0cf5a1c7ededcb05
Fiote de todos <peresdossantosdiego@gmail.com>
2025-06-16 11:42:59 -0300

    Update api.js

--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: 'clinica-geralperes-abcff5fsf2c7hxag.brazilsouth-01.azurewebsites.net',
 });
 
 export default api;
